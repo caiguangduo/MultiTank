42463dfd87e7feee640c58daab108c4405947108
蔡广夺 <1064805848@qq.com>
2018-02-06 19:50:42 +0800

    ServerAndClientProject

---
+++ Server/Serv/core/Main.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Serv
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			RoomMgr roomMgr = new RoomMgr ();
+			DataMgr dataMgr = new DataMgr ();
+			ServNet servNet = new ServNet();
+			servNet.proto = new ProtocolBytes ();
+			servNet.Start("127.0.0.1",1234);
+
+			while(true)
+			{
+				string str = Console.ReadLine();
+				switch(str)
+				{
+				case "quit":
+					servNet.Close();
+					return;
+				case "print":
+					servNet.Print();
+					break;
+				}
+			}
+
+		}
+	}
+}
